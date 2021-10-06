0148798dd0250abd5dd780d36803ffa17e10f93f
Tony Batts <tonybatts@gmail.com>
2021-10-05 18:07:01 -0700

    Updates
    Changelog:
    - Updated Manifest.json to use v3
    - Refactored code
    - Added error handling
    - Added clickable search icon
    - Added trending feature
    - Replaced static gifs used for when there are no gifs for the searched term with the Giphy random API
    - Replaced png icons with SVG
    - Updated styles

--- popup.js
+++ popup.js
@@ -1,116 +1,104 @@
-// automatically focus input when popup is opened
+// Automatically focus input when popup is opened
 document.querySelector("input").focus();
 
 // Initialize state
 const limit = 50;
 let userInput = "";
 let total = 0;
+let userIsOnTrending = false;
 
-// Gifs used when there are no results
-const oopsGifs = [
-  "https://media0.giphy.com/media/AAsj7jdrHjtp6/giphy.gif?cid=182a4348jfwti0jl6ak7m0krwue26dzfvh11qopb270bguug&rid=giphy.gif&ct=g",
-  "https://media3.giphy.com/media/jpbnoe3UIa8TU8LM13/giphy.gif?cid=182a4348o24dqxbn5fm7f9568ctlson1a9fs2ex8ycyk4651&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/54Vj1kxvgyF4k/giphy.gif?cid=182a4348gbltento7lxsg2j5yg3eokhy3smbb6jfne7wtyei&rid=giphy.gif&ct=g",
-  "https://media2.giphy.com/media/olAik8MhYOB9K/giphy.gif?cid=182a4348jfwti0jl6ak7m0krwue26dzfvh11qopb270bguug&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/cF7QqO5DYdft6/giphy.gif?cid=182a4348jfwti0jl6ak7m0krwue26dzfvh11qopb270bguug&rid=giphy.gif&ct=g",
-  "https://media2.giphy.com/media/111ebonMs90YLu/giphy.gif?cid=182a4348jfwti0jl6ak7m0krwue26dzfvh11qopb270bguug&rid=giphy.gif&ct=g",
-  "https://media0.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif?cid=182a4348v2md4cbo7vffdpjfzpvzw7w95kdhgew6gufsoz2z&rid=giphy.gif&ct=g",
-  "https://media4.giphy.com/media/o0vwzuFwCGAFO/giphy.gif?cid=182a4348jfwti0jl6ak7m0krwue26dzfvh11qopb270bguug&rid=giphy.gif&ct=gcat2.gif",
-  "https://media0.giphy.com/media/tvU9iTev6uBIQ/giphy.gif?cid=182a43483wo1z7u13dql1ftfljnn03vb7fnqyw6eiq6dthud&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/v0eHX3n28wvoQ/giphy.gif?cid=182a4348jyh6an2korkw8m2wlbnep3u42m80upvzsbrsfgoo&rid=giphy.gif&ct=g",
-  "https://media0.giphy.com/media/l3q2K5jinAlChoCLS/giphy.gif?cid=182a4348v2md4cbo7vffdpjfzpvzw7w95kdhgew6gufsoz2z&rid=giphy.gif&ct=g",
-  "https://media2.giphy.com/media/l0IylOPCNkiqOgMyA/giphy.gif?cid=182a4348plfr05yv1wv0t320srz1eas5ijhll5jzfv16x1yp&rid=giphy.gif&ct=g",
-  "https://media3.giphy.com/media/W0QduXZQEcNEa8r0oY/giphy.gif?cid=182a43487i0wek45juxhkgrw9pdyixjn5ns0ycbbbitcjizz&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/20k1punZ5bpmM/giphy.gif?cid=182a4348sx75np3xn0f5u3qa74o0bbyvy1cr1roqmmyy3wfo&rid=giphy.gif&ct=g",
-  "https://media4.giphy.com/media/xUPGcvrXl7RBEp3zC8/giphy.gif?cid=182a434869cz5xp8h6v76ff4vvl538gpqt8htc97xf5f2qsu&rid=giphy.gif&ct=g",
-  "https://media2.giphy.com/media/B37cYPCruqwwg/giphy.gif?cid=182a4348f1rym9ed8nsla9rfb28v5qn0titsqlevihzrdtn0&rid=giphy.gif&ct=g",
-  "https://media0.giphy.com/media/LSmULmByAQHQs/giphy.gif?cid=182a43485b646qzlg2nuiig5p2oo0ib7aolr3gdelgnrwtjg&rid=giphy.gif&ct=g",
-  "https://media0.giphy.com/media/3ornk6UHtk276vLtkY/giphy.gif?cid=182a43485b646qzlg2nuiig5p2oo0ib7aolr3gdelgnrwtjg&rid=giphy.gif&ct=g",
-  "https://media4.giphy.com/media/Jn9Td3EAh6cJfiyg60/giphy.gif?cid=182a43483udg5pfkwcx9bsjxhenbn890848kmt5q8tbpj75s&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/a93jwI0wkWTQs/giphy.gif?cid=182a4348kxk8fzhrnhtdg5a00js4ssss7owwt3n8dsmwfm05&rid=giphy.gif&ct=g",
-  "https://media4.giphy.com/media/baPIkfAo0Iv5K/giphy.gif?cid=182a4348kfkn1zs3fuioos7napeou6cqzrc1itt07papqcwt&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/SUzPI5wAkp6UXMEkok/giphy.gif?cid=182a4348kfkn1zs3fuioos7napeou6cqzrc1itt07papqcwt&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/gfO3FcnL8ZK9wVgr6t/giphy.gif?cid=182a4348vpa2h9ufibofbh7gvh2rw0y1ekmxzn7dyr23w6l4&rid=giphy.gif&ct=g",
-  "https://media2.giphy.com/media/dwRwAXIe0tkz0EWFTr/giphy.gif?cid=182a4348vpa2h9ufibofbh7gvh2rw0y1ekmxzn7dyr23w6l4&rid=giphy.gif&ct=g",
-  "https://media3.giphy.com/media/26hkhPJ5hmdD87HYA/giphy.gif?cid=182a4348za7a4spgeh71y38r1xl505n6z4am023zki2enke9&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/OSuaE6AknuRc7syZXp/giphy.gif?cid=182a4348za7a4spgeh71y38r1xl505n6z4am023zki2enke9&rid=giphy.gif&ct=g",
-  "https://media3.giphy.com/media/Az1CJ2MEjmsp2/giphy.gif?cid=182a4348za7a4spgeh71y38r1xl505n6z4am023zki2enke9&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/giXLnhxp60zEEIkq8K/giphy.gif?cid=182a4348za7a4spgeh71y38r1xl505n6z4am023zki2enke9&rid=giphy.gif&ct=g",
-  "https://media3.giphy.com/media/3o7bu7Xzqkq8K3MsUg/giphy.gif?cid=182a4348vvhd4n24dw6o2c5m0vqrbxsrwtaaer7f8r4gykii&rid=giphy.gif&ct=g",
-  "https://media1.giphy.com/media/3orieMhcr3rkME9hkI/giphy.gif?cid=182a4348vvhd4n24dw6o2c5m0vqrbxsrwtaaer7f8r4gykii&rid=giphy.gif&ct=g"
-];
+// Display error message
+const displayError = (errorMessage) => {
+  const errorMessageWrapperEl = document.createElement("div");
+  const errorMessageEl = document.createElement("h1");
 
-// used to pick random gif for no results view
-const getRandomNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  errorMessageWrapperEl.classList.add("center-no-results");
+  errorMessageWrapperEl.style.height = "150px";
+  errorMessageEl.classList.add("no-results-small");
+  errorMessageEl.textContent = errorMessage;
+
+  errorMessageWrapperEl.appendChild(errorMessageEl);
+  document.querySelector(".popup-container").appendChild(errorMessageWrapperEl);
 };
 
 // Use the current input to load more gifs
 const loadGifs = async () => {
   userInput = document.querySelector("input").value;
 
-  if (userInput.length === 0) {
-    // return if user cleared input and hit enter
+  // Return if user cleared input and hit enter
+  if (userInput.length === 0 && !userIsOnTrending) {
     return;
   }
 
-  try {
-    // call the API to get gifs
-    await getSearchResults(userInput);
-  } catch (error) {
-    console.log(error.message);
+  // Get trending gifs
+  if (userIsOnTrending) {
+    await getTrendingResults();
+    return;
   }
+
+  // Get search term gifs
+  await getSearchResults(userInput);
 };
 
-const noResults = () => {
+// Handle when there are no results for search term
+const noResults = async (
+  string = "No gifs found. Try searching for something else."
+) => {
+  const sad = await getRandomGif("oops");
   document.querySelector(".popup-container").textContent = "";
 
-  const randomNum = getRandomNumber(0, oopsGifs.length - 1);
-  const noResultWrapperEl = document.createElement("div");
-  const noResultEl = document.createElement("h1");
+  const errorMessageWrapperEl = document.createElement("div");
+  const errorMessageEl = document.createElement("h1");
+  const figureEl = document.createElement("figure");
+  const figcapEl = document.createElement("figcaption");
   const imgEl = document.createElement("img");
 
   imgEl.classList.add("oops-gif");
-  imgEl.src = oopsGifs[randomNum];
-  noResultWrapperEl.classList.add("center-no-results");
-  noResultEl.classList.add("no-results-small");
-  noResultEl.textContent = "No results found :( Try another search term";
+  imgEl.src = sad;
+  figcapEl.textContent = 'Randomly generated "oops" gif';
+  figcapEl.classList.add("figcaption");
+  errorMessageWrapperEl.classList.add("center-no-results");
+  errorMessageEl.classList.add("no-results-small");
+  errorMessageEl.textContent = string;
 
-  noResultWrapperEl.appendChild(noResultEl);
-  noResultWrapperEl.appendChild(imgEl);
-  document.querySelector(".popup-container").appendChild(noResultWrapperEl);
+  figureEl.appendChild(imgEl);
+  figureEl.appendChild(figcapEl);
+  errorMessageWrapperEl.appendChild(errorMessageEl);
+  errorMessageWrapperEl.appendChild(figureEl);
+  document.querySelector(".popup-container").appendChild(errorMessageWrapperEl);
 };
 
+// Display message when there are no more gifs to load for the current search term
 const endOfList = () => {
   const noResultsEl = document.querySelector(".center-no-results");
+  const inputEl = document.querySelector("input");
   if (noResultsEl) {
     noResultsEl.remove();
   }
 
-  const noResultWrapperEl = document.createElement("div");
-  const noResultEl = document.createElement("h1");
+  const errorMessageWrapperEl = document.createElement("div");
+  const errorMessageEl = document.createElement("h1");
 
-  noResultWrapperEl.classList.add("center-no-results");
-  noResultWrapperEl.style.height = "150px";
-  noResultEl.classList.add("no-results-small");
-  noResultEl.textContent = "No more results for this search term";
+  errorMessageWrapperEl.classList.add("center-no-results");
+  errorMessageWrapperEl.style.height = "150px";
+  errorMessageEl.classList.add("no-results-small");
+  errorMessageEl.textContent = `No more results for "${inputEl.value}"`;
 
-  noResultWrapperEl.appendChild(noResultEl);
-  document.querySelector(".popup-container").appendChild(noResultWrapperEl);
+  errorMessageWrapperEl.appendChild(errorMessageEl);
+  document.querySelector(".popup-container").appendChild(errorMessageWrapperEl);
 };
 
-// Use the data from the API to generate the list of gifs and append to dom
+// Generate the list of gifs and append to dom
 const generateList = ({ data }) => {
   total = total + data.length;
 
-  // return if user reaches end of list and there are no more results to load
+  // Return if user reaches end of list and there are no more results to load
   if (total > 0 && data.length === 0) {
-    console.log("end of list");
     endOfList();
     return;
   }
 
-  // handle no results
+  // Handle when there are no results for search term
   if (data.length === 0) {
     noResults();
     return;
@@ -129,36 +117,100 @@ const generateList = ({ data }) => {
   });
 };
 
-// make API request for gif data
+// Get search results
 const getSearchResults = async (searchTerm) => {
-  if (searchTerm.length === 0) {
-    noResults();
+  if (searchTerm.length === 0 && userIsOnTrending === false) {
+    noResults("Don't forget to enter a search term");
     return;
   }
 
-  const response = await fetch(
-    `http://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY&q=${searchTerm}&limit=${limit}&offset=${total}`
-  );
+  try {
+    const response = await fetch(
+      `http://api.giphy.com/v1/gifs/search?api_key=H2vDwH21VkkjmAKNUMQUz0gB1omdiDCf&q=${searchTerm}&limit=${limit}&offset=${total}`
+    );
 
-  console.log("fetched");
+    if (!response.ok) {
+      throw new Error(`status ${response.status}`);
+    }
 
-  if (!response.ok) {
-    throw new Error(`An error occured: ${response.status}`);
+    const parsedData = await response.json();
+
+    generateList(parsedData);
+  } catch (error) {
+    displayError(error);
   }
+};
 
-  const parsedData = await response.json();
+// Get trending gifs
+const getTrendingResults = async () => {
+  try {
+    const response = await fetch(
+      `http://api.giphy.com/v1/gifs/trending?api_key=H2vDwH21VkkjmAKNUMQUz0gB1omdiDCf&&limit=${limit}&offset=${total}`
+    );
 
-  generateList(parsedData);
+    if (!response.ok) {
+      throw new Error(`An error occured: ${response.status}`);
+    }
+
+    const parsedData = await response.json();
+
+    generateList(parsedData);
+  } catch (error) {
+    displayError(error);
+  }
+};
+
+// Get random gif
+const getRandomGif = async (keyWord) => {
+  try {
+    const response = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=H2vDwH21VkkjmAKNUMQUz0gB1omdiDCf&&tag=${keyWord}&rating=g`
+    );
+
+    if (!response.ok) {
+      throw new Error(`An error occured: ${response.status}`);
+    }
+
+    const parsedData = await response.json();
+
+    return parsedData.data.images.original.url;
+  } catch (error) {
+    console.log(error);
+    // Return a fallback gif when there is a problem with the API request
+    return "https://media4.giphy.com/media/a5viI92PAF89q/giphy.gif?cid=182a43489cmtjmbk2e5sopnxihvjas1v5qb92tauc8m9sjka&rid=giphy.gif&ct=g";
+  }
 };
 
 // When user hits enter call start the data flow
 document.querySelector("input").addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
+    userIsOnTrending = false;
     total = 0;
     document.querySelector(".popup-container").textContent = "";
     window.scrollTo(0, 0);
     getSearchResults(e.target.value);
   }
+});
+
+// When user clicks search icon start data flow
+document.querySelector(".search-icon").addEventListener("click", (e) => {
+  const searchInput = document.querySelector("input");
+  searchInput.focus();
+  userIsOnTrending = false;
+  total = 0;
+  document.querySelector(".popup-container").textContent = "";
+  window.scrollTo(0, 0);
+  getSearchResults(searchInput.value);
+});
+
+// When user clicks trending icon start data flow
+document.querySelector(".trending-hover").addEventListener("click", (e) => {
+  document.querySelector("input").focus();
+  userIsOnTrending = true;
+  total = 0;
+  document.querySelector(".popup-container").textContent = "";
+  window.scrollTo(0, 0);
+  getTrendingResults();
 });
 
 // Infinite scroll
@@ -168,11 +220,10 @@ document.addEventListener(
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
     if (
-      scrollTop + clientHeight >= scrollHeight - 5 &&
+      scrollTop + clientHeight >= scrollHeight - 0 &&
       !document.querySelector(".center-no-results") &&
       total !== 0
     ) {
-      console.log("bottom of page, loading more gifs");
       loadGifs();
     }
   },
@@ -180,5 +231,3 @@ document.addEventListener(
     passive: true
   }
 );
-
-// chrome.storage.sync.set({ hex2: true });
